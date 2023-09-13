7b5dfad2e7ad20ac3fbc1af0ff566e307ca75e23
Malik Kotb <malikkotb@icloud.com>
2023-09-13 08:52:53 +0200

    change material on collision

--- Assets/ColorChanger.cs
+++ Assets/ColorChanger.cs
@@ -1,9 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorChanger : MonoBehaviour
 {
+
+    // set movementspeed of capsule
+    private float movementSpeed = 2f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +15,13 @@ public class ColorChanger : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        transform.position += new Vector3(movementSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        var renderer = this.GetComponent<Renderer>();
+        renderer.material = collision.collider.GetComponent<Renderer>().material;
         
     }
 }
